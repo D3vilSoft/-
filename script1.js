a2ff35f81b57d4b627eeab9b60c8811810cc7df0
D3vilSoft <jzsmshsjssj@gmail.com>
2024-11-21 17:14:24 +0300

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,4 +1,4 @@
-document.getElementById('color-button').addEventListener('click', () => {
+document.getElementById('rotateButton').addEventListener('click', () => {
     const imageContainer = document.getElementById('imageContainer');
     const images = Array.from(document.querySelectorAll('.small-image'));
     let count = 0;
